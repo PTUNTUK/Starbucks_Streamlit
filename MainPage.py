fe3bb09cd10607485ea0e6dbe75aee5e3026bd7e
Pornchanok.T <pornchanok.tan14@gmail.com>
2025-04-27 22:10:43 +0700

    Add files via upload

--- MainPage.py
+++ MainPage.py
@@ -9,216 +9,133 @@ def intro():
 
     df = pd.read_csv("Starbucks.csv")
     st.write(df)
-    #x = 'Bangkok'
-    x = st.sidebar.selectbox('Choose your city',['Bangkok','Ajman','Phuket'])
-    results = db.sql(f"SELECT * FROM df where City='{x}'").df()
-    st.write(results)
 
-    st.markdown(
-        """
-        1 intro
-        """
-    )
-
-#create new def
-def intro2():
-    st.sidebar.write("hi")
-
-def mapping_demo():
+def ownership_demo():
     import streamlit as st
+    import duckdb as db
     import pandas as pd
     import pydeck as pdk
+    import plotly.express as px
 
-    from urllib.error import URLError
+    st.write("# Welcome to Streamlit! 👋")
 
-    st.markdown(f"# {list(page_names_to_funcs.keys())[2]}")
-    st.write(
-        """
-        This demo shows how to use
-[`st.pydeck_chart`](https://docs.streamlit.io/develop/api-reference/charts/st.pydeck_chart)
-to display geospatial data.
-"""
-    )
+    df = pd.read_csv("Starbucks.csv")
+    #st.write(df)
 
-    @st.cache_data
-    def from_data_file(filename):
-        url = (
-            "http://raw.githubusercontent.com/streamlit/"
-            "example-data/master/hello/v1/%s" % filename
-        )
-        return pd.read_json(url)
+    #Result1 - Ownership type
+    results1 = db.sql(f'SELECT  "Ownership Type" as Ownership  , COUNT(*) as Total FROM df GROUP BY "Ownership Type" ORDER BY Total DESC').df()
+    fig = px.bar(results1, x='Ownership', y='Total')
+    st.subheader('Ownership Type📊')
+    st.write(results1)
+    st.plotly_chart(fig)
 
-    try:
-        ALL_LAYERS = {
-            "Bike Rentals": pdk.Layer(
-                "HexagonLayer",
-                data=from_data_file("bike_rental_stats.json"),
-                get_position=["lon", "lat"],
-                radius=200,
-                elevation_scale=4,
-                elevation_range=[0, 1000],
-                extruded=True,
-            ),
-            "Bart Stop Exits": pdk.Layer(
-                "ScatterplotLayer",
-                data=from_data_file("bart_stop_stats.json"),
-                get_position=["lon", "lat"],
-                get_color=[200, 30, 0, 160],
-                get_radius="[exits]",
-                radius_scale=0.05,
-            ),
-            "Bart Stop Names": pdk.Layer(
-                "TextLayer",
-                data=from_data_file("bart_stop_stats.json"),
-                get_position=["lon", "lat"],
-                get_text="name",
-                get_color=[0, 0, 0, 200],
-                get_size=15,
-                get_alignment_baseline="'bottom'",
-            ),
-            "Outbound Flow": pdk.Layer(
-                "ArcLayer",
-                data=from_data_file("bart_path_stats.json"),
-                get_source_position=["lon", "lat"],
-                get_target_position=["lon2", "lat2"],
-                get_source_color=[200, 30, 0, 160],
-                get_target_color=[200, 30, 0, 160],
-                auto_highlight=True,
-                width_scale=0.0001,
-                get_width="outbound",
-                width_min_pixels=3,
-                width_max_pixels=30,
-            ),
-        }
-        st.sidebar.markdown("### Map Layers")
-        selected_layers = [
-            layer
-            for layer_name, layer in ALL_LAYERS.items()
-            if st.sidebar.checkbox(layer_name, True)
-        ]
-        if selected_layers:
-            st.pydeck_chart(
-                pdk.Deck(
-                    map_style="mapbox://styles/mapbox/light-v9",
-                    initial_view_state={
-                        "latitude": 37.76,
-                        "longitude": -122.4,
-                        "zoom": 11,
-                        "pitch": 50,
-                    },
-                    layers=selected_layers,
-                )
-            )
-        else:
-            st.error("Please choose at least one layer above.")
-    except URLError as e:
-        st.error(
-            """
-            **This demo requires internet access.**
+    #Result 4 - Percentage Ownership Pie Chart
+    result_4 = db.sql(f'SELECT "Ownership Type" As "Ownership", count(*) as count FROM df group by "Ownership Type" ').df()
+    st.subheader('Percentage Ownership Pie Chart🥧')
 
-            Connection error: %s
-        """
-            % e.reason
-        )
+    #st.write(result_4)
+    fig_3 = px.pie(result_4, values='count', names='Ownership', title='Ownership Type')
+    st.plotly_chart(fig_3)
 
-def plotting_demo():
+def top10_demo():
     import streamlit as st
-    import time
-    import numpy as np
-
-    st.markdown(f'# {list(page_names_to_funcs.keys())[1]}')
-    st.write(
-        """
-        This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!
-"""
-    )
-
-    progress_bar = st.sidebar.progress(0)
-    status_text = st.sidebar.empty()
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
-
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        status_text.text("%i%% Complete" % i)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
-
-    progress_bar.empty()
-
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
-
-def data_frame_demo():
-    import streamlit as st
+    import duckdb as db
     import pandas as pd
-    import altair as alt
+    import pydeck as pdk
+    import plotly.express as px
 
-    from urllib.error import URLError
+    st.write("# Welcome to Streamlit! 👋")
+    #st.sidebar.success("Select a demo above.")
 
-    st.markdown(f"# {list(page_names_to_funcs.keys())[3]}")
-    st.write(
-        """
-        This demo shows how to use `st.write` to visualize Pandas DataFrames.
+    df = pd.read_csv("Starbucks.csv")
+    #st.write(df)
 
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)
-"""
+    #Result2 - Count By Country
+    result_2 = db.sql(f"SELECT Country, count(*) as count FROM df group by Country order by count desc limit 10").df()
+    st.subheader('👑Top 10 Country by Number of Starbucks')
+    #st.write(result_2)
+    
+    fig = px.bar(result_2, x='Country', y='count')
+    st.plotly_chart(fig)
+
+def map_demo():
+    import streamlit as st
+    import duckdb as db
+    import pandas as pd
+    import pydeck as pdk
+    import plotly.express as px
+
+    st.write("# Welcome to Streamlit! 👋")
+    #st.sidebar.success("Select a demo above.")
+
+    df = pd.read_csv("Starbucks.csv")
+    #st.write(df)
+
+    #Results3 - Map
+    results3 = db.sql("""
+        SELECT Latitude, Longitude, COUNT(*) AS shop_count
+        FROM df
+        GROUP BY Latitude, Longitude
+    """).df()
+
+    #st.write(results3)
+
+    st.subheader('🌏Starbucks Map by Location')
+    st.pydeck_chart(
+        pdk.Deck(
+            initial_view_state=pdk.ViewState(
+                latitude=results3["Latitude"].mean(),  
+                longitude=results3["Longitude"].mean(),
+                zoom=2,
+                pitch=50,
+            ),
+            layers=[
+                pdk.Layer(
+                    "ScatterplotLayer",
+                    data=results3,
+                    get_position='[Longitude, Latitude]',
+                    get_radius='shop_count * 1000',  
+                    get_fill_color='[200, 30, 0, 160]',
+                    pickable=True,
+                )
+            ],
+        )
     )
 
-    @st.cache_data
-    def get_UN_data():
-        AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-        df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-        return df.set_index("Region")
+    #Results5 - Plotly Map
+    st.subheader('🚩Number of Starbucks by Location')
 
-    try:
-        df = get_UN_data()
-        countries = st.multiselect(
-            "Choose countries", list(df.index), ["China", "United States of America"]
-        )
-        if not countries:
-            st.error("Please select at least one country.")
-        else:
-            data = df.loc[countries]
-            data /= 1000000.0
-            st.write("### Gross Agricultural Production ($B)", data.sort_index())
+    # Create scatter_mapbox with clustering and black color
+    fig = px.scatter_mapbox(
+        results3,
+        lat="Latitude",
+        lon="Longitude",
+        #size="shop_count",
+        #size_max=30, 
+        hover_data={"shop_count": True},
+        zoom=1.5,
+        height=600,
+    )
 
-            data = data.T.reset_index()
-            data = pd.melt(data, id_vars=["index"]).rename(
-                columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-            )
-            chart = (
-                alt.Chart(data)
-                .mark_area(opacity=0.3)
-                .encode(
-                    x="year:T",
-                    y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                    color="Region:N",
-                )
-            )
-            st.altair_chart(chart, use_container_width=True)
-    except URLError as e:
-        st.error(
-            """
-            **This demo requires internet access.**
+    # Use open-source map style
+    fig.update_layout(mapbox_style="open-street-map")
 
-            Connection error: %s
-        """
-            % e.reason
-        )
+    fig.update_traces(
+        cluster=dict(enabled=True),
+        marker=dict(color="green")
+    )
+
+    st.plotly_chart(fig)
 
 page_names_to_funcs = {
-    "my_intro": intro,
-    "Plotting Demo": plotting_demo,
-    "Mapping Demo": mapping_demo,
-    "DataFrame Demo": data_frame_demo,
-    "my_page2": intro2
+    #"my_intro": intro,
+    #"Plotting Demo": plotting_demo,
+    #"Mapping Demo": mapping_demo,
+    #"DataFrame Demo": data_frame_demo,
+    #"my_page2": intro2,
+    "Ownership" : ownership_demo,
+    "👑Top 10 Country" : top10_demo,
+    "🌏Mapping" : map_demo
 }
 
 st.sidebar.write("my sidebar")
